3edcfa3387b900c1f739d02da3443120c0568120
amd <amd@devin.com>
2023-06-27 17:12:08 +0900

    오타 수정.

--- src/springbook/user/dao/UserDaoTest.java
+++ src/springbook/user/dao/UserDaoTest.java
@@ -35,7 +35,7 @@ public class UserDaoTest {
     }
 
     @Test
-    public void andAndGet() throws SQLException {
+    public void addAndGet() throws SQLException {
         dao.deleteAll();
         assertThat(dao.getCount(), is(0));
 
