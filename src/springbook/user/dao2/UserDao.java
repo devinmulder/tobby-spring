01a76472592580d2814c5d2ea8a114255c562813
amd <amd@devin.com>
2023-07-01 20:40:18 +0900

    260 page : JdbcTemplate 도입

---
+++ src/springbook/user/dao2/UserDao.java
@@ -0,0 +1,45 @@
+package springbook.user.dao2;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCreator;
+import springbook.user.dao.JdbcContext;
+import springbook.user.domain.User;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class UserDao {
+    private DataSource dataSource;
+
+    public void setDataSource(DataSource dataSource) {
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
+
+        this.dataSource = dataSource;
+    }
+
+    private JdbcTemplate jdbcTemplate;
+
+    public void add(final User user) throws SQLException {
+    }
+
+    public User get(String id) throws SQLException {
+        return null;
+    }
+
+    public void deleteAll() throws SQLException {
+        this.jdbcTemplate.update(
+                new PreparedStatementCreator() {
+                    @Override
+                    public PreparedStatement createPreparedStatement(Connection con) throws SQLException {
+                        return con.prepareStatement("delete from users");
+                    }
+                }
+        );
+    }
+
+    public int getCount() throws SQLException  {
+        return 0;
+    }
+}
