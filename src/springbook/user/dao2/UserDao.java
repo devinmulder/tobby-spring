1149d48e286cd3746ad4aa96095bc134ccf17eab
amd <amd@devin.com>
2023-07-01 21:02:12 +0900

    262-265 page

--- src/springbook/user/dao2/UserDao.java
+++ src/springbook/user/dao2/UserDao.java
@@ -1,13 +1,17 @@
 package springbook.user.dao2;
 
+import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.PreparedStatementCreator;
+import org.springframework.jdbc.core.ResultSetExtractor;
+import org.springframework.jdbc.core.RowMapper;
 import springbook.user.dao.JdbcContext;
 import springbook.user.domain.User;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class UserDao {
@@ -22,10 +26,25 @@ public class UserDao {
     private JdbcTemplate jdbcTemplate;
 
     public void add(final User user) throws SQLException {
+        this.jdbcTemplate.update("insert into users(id, name, password) values(?,?,?)",
+                user.getId(),
+                user.getName(),
+                user.getPassword());
     }
 
     public User get(String id) throws SQLException {
-        return null;
+        return this.jdbcTemplate.queryForObject("select * from users where id = ?",
+                new Object[]{id},
+                new RowMapper<User>() {
+                    @Override
+                    public User mapRow(ResultSet rs, int i) throws SQLException {
+                        User user = new User();
+                        user.setId(rs.getString("id"));
+                        user.setName(rs.getString("name"));
+                        user.setPassword(rs.getString("password"));
+                        return user;
+                    }
+                });
     }
 
     public void deleteAll() throws SQLException {
@@ -33,6 +52,20 @@ public class UserDao {
     }
 
     public int getCount() throws SQLException  {
-        return 0;
+        return this.jdbcTemplate.query(
+                new PreparedStatementCreator() {
+                    @Override
+                    public PreparedStatement createPreparedStatement(Connection con) throws SQLException {
+                        return con.prepareStatement("select count(*) from users");
+                    }
+                },
+                new ResultSetExtractor<Integer>() {
+                    @Override
+                    public Integer extractData(ResultSet rs) throws SQLException, DataAccessException {
+                        rs.next();
+                        return rs.getInt(1);
+                    }
+                }
+        );
     }
 }
