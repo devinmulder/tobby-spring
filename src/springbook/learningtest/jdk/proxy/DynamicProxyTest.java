6bbe1b0337255089cab120d9b914110d3dc1a8fb
amd <amd@devin.com>
2023-07-07 11:14:30 +0900

    469 page

--- src/springbook/learningtest/jdk/proxy/DynamicProxyTest.java
+++ src/springbook/learningtest/jdk/proxy/DynamicProxyTest.java
@@ -11,6 +11,8 @@ import org.aopalliance.intercept.MethodInterceptor;
 import org.aopalliance.intercept.MethodInvocation;
 import org.junit.Test;
 import org.springframework.aop.framework.ProxyFactoryBean;
+import org.springframework.aop.support.DefaultPointcutAdvisor;
+import org.springframework.aop.support.NameMatchMethodPointcut;
 
 
 public class DynamicProxyTest {
@@ -91,6 +93,25 @@ public class DynamicProxyTest {
             return ret.toUpperCase();
         }
     }
+
+
+    @Test
+    public void pointcutAdvisor() {
+        ProxyFactoryBean pfBean = new ProxyFactoryBean();
+        pfBean.setTarget(new HelloTarget());
+
+        NameMatchMethodPointcut pointcut = new NameMatchMethodPointcut();
+        pointcut.setMappedName("sayH*");
+
+        pfBean.addAdvisor(new DefaultPointcutAdvisor(pointcut, new UppercaseAdvice()));
+
+        Hello proxiedHello = (Hello) pfBean.getObject();
+
+        assertThat(proxiedHello.sayHello("Toby"), is("HELLO TOBY"));
+        assertThat(proxiedHello.sayHi("Toby"), is("HI TOBY"));
+        assertThat(proxiedHello.sayThankYou("Toby"), is("Thank You Toby"));
+    }
+
 
     static interface Hello {
         String sayHello(String name);
